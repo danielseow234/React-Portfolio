7e80598be3a05eeb9670a395f32eee48413665de
danielseow234 <danielseow234@gmail.com>
2022-05-09 00:13:37 +0800

    Importing languages icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,11 @@ export default function About(){
     return (
         <div className='about shared' id='about'>
             <div className='inner'>
-                <div className='sub'>
-                    Languages
+                <div className='sub'> 
+                    <i class="devicon-javascript-plain"></i>
+                    <i class="devicon-python-plain"></i>
+                    <i class="devicon-react-original"></i>
+                    <i class="devicon-nodejs-plain"></i>
                 </div>
                 <div className='sub'>
                     soft skills
