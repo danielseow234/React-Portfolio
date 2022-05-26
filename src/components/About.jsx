51b408873e667175ff92ce856e90178cb063d8f1
danielseow234 <danielseow234@gmail.com>
2022-05-27 02:39:36 +0800

    fixed icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,33 @@ export default function About(){
     return (
         <div className='about shared' id='about'>
             <div className='inner'>
-                <div className='sub'>
-                    <h1>Some languages I'm familiar with</h1>
-                    <i className="icon devicon-javascript-plain"></i>
-                    <i className="icon devicon-python-plain"></i>
-                    <i className="icon devicon-react-original"></i>
-                    <i className="icon devicon-nodejs-plain"></i>
+                <div className='sub sub-left'>
+                    <h1 className='sub-title'>Languages and Frameworks</h1>
+                    <div className='lang-div'>
+                        <div className='lang'>
+                            <i className="icon devicon-javascript-plain"></i>
+                            <p className='lang-txt'>7/10</p>
+                        </div>
+                        <div className='lang'>
+                            <i className="icon devicon-python-plain"></i>
+                            <p className='lang-txt'>6/10</p>
+                        </div>
+                        <div className='lang'>
+                            <i className="icon devicon-react-plain"></i>
+                            <p className='lang-txt'>8/10</p>
+                        </div>
+                        <div className='lang'>
+                            <i className="icon devicon-nodejs-plain"></i>
+                            <p className='lang-txt'>6/10</p>
+                        </div>
+                    </div>
                 </div>
                 <div className='sub'>
-                    soft skills
+                    <h1 className='sub-title'>Soft skills</h1>
+                    <p>communication skills</p>
+                    <p>learning skills</p>
+                    <p>adaptable</p>
+                    <p>teamwork</p>
                 </div>
             </div>
         </div> 
