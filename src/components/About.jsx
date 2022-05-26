7b183b223de6865c48033045a3b3566cb0797764
danielseow234 <danielseow234@gmail.com>
2022-05-27 00:33:35 +0800

    short changes to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,12 @@ export default function About(){
     return (
         <div className='about shared' id='about'>
             <div className='inner'>
-                <div className='sub'> 
-                    <i class="devicon-javascript-plain"></i>
-                    <i class="devicon-python-plain"></i>
-                    <i class="devicon-react-original"></i>
-                    <i class="devicon-nodejs-plain"></i>
+                <div className='sub'>
+                    <h1>Some languages I'm familiar with</h1>
+                    <i className="icon devicon-javascript-plain"></i>
+                    <i className="icon devicon-python-plain"></i>
+                    <i className="icon devicon-react-original"></i>
+                    <i className="icon devicon-nodejs-plain"></i>
                 </div>
                 <div className='sub'>
                     soft skills
@@ -18,4 +19,3 @@ export default function About(){
         </div> 
     )
 }
-
